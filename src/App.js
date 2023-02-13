b0bc3ef9908e4906c610539f1dc067b47f43ec41
akashmhaiskar <akashmhaiskar1@gmail.com>
2023-02-13 21:48:42 +0530

    Added react router dom

--- src/App.js
+++ src/App.js
@@ -1,14 +1,20 @@
-import logo from './logo.svg';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import Base from './components/Base';
+import {BrowserRouter,Routes,Route} from 'react-router-dom';
+import Home from './pages/Home';
+import About from './pages/About';
+import Login from './pages/Login';
+import Signup from './pages/Signup';
 function App() {
   return (
-                  <Base>
-
-                  <h1>This is app components</h1>
-
-                  </Base>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home/>} />
+        <Route path="/login" element={<Login/>} />
+        <Route path="/signup" element={<Signup/>} />
+        <Route path="/about" element={<About/>} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
---
+++ src/pages/Signup.js
@@ -0,0 +1,11 @@
+const Signup = () => {
+    return (
+        <div>
+            <h1>This is signup page</h1>
+            <p>Welcome to signup page</p>
+        </div>
+    );
+
+}
+
+export default Signup;
