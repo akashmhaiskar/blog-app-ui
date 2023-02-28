f8a9763fe2fcab6fd930ce8395580032e9e0ed9d
akashmhaiskar <akashmhaiskar1@gmail.com>
2023-02-28 20:10:30 +0530

    Added login functionality

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -11,9 +11,61 @@ import {
   Row,
   Col,
 } from "reactstrap";
+import { useEffect, useState } from "react";
+import {toast } from 'react-toastify';
 import Base from "../components/Base";
+import { loginUser } from "../services/user-service";
 
 const Login = () => {
+
+const [loginDetail, setLoginDetail] = useState({
+  username:'',
+  password:''
+});
+
+const handleChange = (event, field)=>{
+
+  let actualValue=event.target.value
+  setLoginDetail({
+    ...loginDetail,
+    [field]:actualValue
+  })
+}
+
+const handleReset = () => {
+  setLoginDetail({
+    username:"",
+    password:""
+  });
+};
+
+
+const handleFormSubmit = (event) =>{
+  event.preventDefault();
+  console.log(loginDetail);
+
+  //validation
+  if(loginDetail.username.trim() == '' || loginDetail.password.trim() == ''){
+    toast.error("Username or password is required !!");
+    return;
+  }
+
+  //submit the data to server to generate token
+  loginUser(loginDetail).then((jwtTokenData) => {
+    console.log("user login: ")
+    console.log(jwtTokenData)
+    toast.success('Login Success !!')
+  }).catch(error => {
+    console.log(error)
+    if(error.response.status == 400 || error.response.status == 404
+      || error.response.status == 401){
+      toast.error(error.response.data.message)
+    }else {
+      toast.error("Something went wrong on server !!")
+    }
+  })
+};
+
   return (
     <Base>
       <Container>
@@ -26,22 +78,26 @@ const Login = () => {
               <CardBody>
                 {/* creating login form*/}
 
-                <Form>
+                <Form onSubmit={handleFormSubmit}>
                   {/*Email field */}
                   <FormGroup>
                     <Label for="email">Enter email</Label>
-                    <Input type="email" placeholder="Enter here" id="email" />
+                    <Input type="email" placeholder="Enter here" id="email" 
+                    value={loginDetail.username}
+                    onChange = {(e) => handleChange(e, 'username')}/>
                   </FormGroup>
                   {/*password field */}
                   <FormGroup>
                     <Label for="password">Enter password</Label>
-                    <Input type="password" id="password" />
+                    <Input type="password" id="password" 
+                     value={loginDetail.password}
+                     onChange = {(e) => handleChange(e, 'password')}/>
                   </FormGroup>
                   <Container className="text-center">
                     <Button color="light" outline>
                       Login
                     </Button>
-                    <Button
+                    <Button onClick={handleReset}
                       color="secondary"
                       type="reset"
                       className="ms-2"
